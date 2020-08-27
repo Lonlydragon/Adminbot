e825acf73069c5a7cc8cfc80fa3ecfe6a975ebc7
Lonlydragon <43724479+Lonlydragon@users.noreply.github.com>
2020-08-27 23:03:32 +0300

    Initial commit

---
+++ bot.py
@@ -0,0 +1,26 @@
+
+import config
+from aiogram import Bot, Dispatcher, executor, types
+
+bot = Bot(token=config.TELEGRAM_API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(content_types=["new_chat_members"])
+async def send_welcome(message: types.message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    user_name = message.from_user.first_name
+    await message.chat.promote(user_id=message.from_user.id, can_delete_messages=False, can_pin_messages=True,
+                                   can_invite_users=True)
+    await message.answer("{} выданы админские права, Lalka".format(user_name))
+    await message.chat.set_administrator_custom_title(user_id=message.from_user.id, custom_title="Lalka")
+
+@dp.message_handler(content_types=["left_chat_member"])
+async def send_welcome(message: types.message):
+    user_name = message.from_user.first_name
+    await message.answer("{} прощай, Lalka".format(user_name))
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
